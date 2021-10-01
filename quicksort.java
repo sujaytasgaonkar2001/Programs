9e78e188ba2e95bf11016e1502894708bb331ded
Sujay S T <71273960+sujaytasgaonkar2001@users.noreply.github.com>
2021-10-01 21:47:07 +0530

    Add files via upload

--- quicksort.java
+++ quicksort.java
@@ -1,63 +1,82 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Random;
-
-public class quicksort {
-	static final int MAX = 10005;
-	static int[] a = new int[MAX];
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter Max array size: ");
-		int n = input.nextInt();
-		Random random = new Random();
-		System.out.println("Enter the array elements: ");
-		for (int i = 0; i < n; i++)
+package project4;
+import java.util.*;
+class Pg8{
+	static void quicksort(int a[], int p, int q)
+	{
+		int j;
+		if(p < q)
 		{
-			//a[i] = input.nextInt();   	                         
-			a[i] = random.nextInt(5000); // generate 
-			// random numbers – uniform distribution
+			j = partition(a, p, q);
+			quicksort(a, p, j-1);
+			quicksort(a, j+1, q);
 		}
-		//a = Arrays.copyOf(a, n); // keep only non zero elements
-		//Arrays.sort(a); // for worst-case time complexity
-		System.out.println("Input Array:");
-		for (int i = 0; i < n; i++)
-			System.out.print(a[i] + " ");
-		// set start time
-		long startTime = System.nanoTime();
-		QuickSortAlgorithm(0, n - 1);
-		long stopTime = System.nanoTime();
-		long elapsedTime = stopTime - startTime;
-		System.out.println("\nSorted Array:");
-		for (int i = 0; i < n; i++)
-			System.out.print(a[i] + " ");
-		System.out.println();
-		System.out.println("Time Complexity in ms for n=" + n + " is: " + (double) elapsedTime / 1000000);
+	}
+	
+	static int partition(int a[], int m, int p)
+	{
+		int i, j;
+		int v = a[m]; 
+		i = m;
+		j = p;
+		while(i <= j) 
+		{
+			while(a[i] <= v)
+			{
+				i++;
+			}
+			while(a[j] > v)
+			{
+				j--;
+			}
+			if(i<j)
+			{
+				swap(a, i, j); 
+			}
+		}
+		a[m] = a[j];
+		a[j] = v;  
+		return j;	
+	}
+	
+	static void swap(int a[], int i, int j)
+	{
+		int p;
+		p = a[i];
+		a[i] = a[j];
+		a[j] = p;
 	}
 
-	public static void QuickSortAlgorithm(int lowerBound, int upperBound) {
-		int start, end, temp, pivot;
-		if (lowerBound < upperBound) {
-			start = lowerBound;
-			end = upperBound + 1;
-			pivot = a[lowerBound]; // mark first element as pivot
-			while (true) {
-				start++;
-				while (a[start] < pivot && start < upperBound)
-					start++;
-				end--;
-				while (a[end] > pivot)
-					end--;
-				if (start < end) {
-					temp = a[start];
-					a[start] = a[end];
-					a[end] = temp;
-				} else
-					break; // partition is over
-			}
-			a[lowerBound] = a[end];
-			a[end] = pivot;
-			QuickSortAlgorithm(lowerBound, end - 1);
-			QuickSortAlgorithm(end + 1, upperBound);
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[] = new int[10000];
+		long start, end;
+		Scanner read = new Scanner(System.in);
+		Random rand = new Random();
+		
+		System.out.println("Enter number of elements:");
+		int n = read.nextInt();
+		System.out.println("\nElements generated");
+		for(int h = 0; h<n; h++)
+		{
+			a[h] = rand.nextInt(10000);
+			System.out.print(a[h]+" ");
 		}
+		a[n] = 9999;
+		
+		start = System.nanoTime();
+		quicksort(a, 0, n-1);
+		end = System.nanoTime();
+		
+		System.out.println("\n\nThe sorted elements are");
+		for(int k = 0; k<n; k++)
+			System.out.print(a[k]+" ");
+		System.out.println("\n\ntime taken = "+(end-start)+"ns");
+		}
+		
+		
+		
+
 	}
-}
